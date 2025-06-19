979ef17a72cb39f29d27253e01106a7705886534
mahendralegion <mahendra@legion.is>
2025-06-19 16:45:05 +0530

    change firbase to staging

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,7 +1,2 @@
-//staging
-//var version = "1.5.1";
-//importScripts("https://cdn.subscribersdemo.com/assets/subscribers-sw.js");
-
-//prod
 var version = "1.5.1";
-importScripts("https://cdn.subscribers.com/assets/subscribers-sw.js");
+importScripts("https://cdn.subscribersdemo.com/assets/subscribers-sw.js");
